9fda01569f564adf892f205670f475a66f296609
semanew <nebiyuessayas01@gmail.com>
2024-04-22 07:36:01 +0300

    finished except icons

---
+++ lib/another.dart
@@ -0,0 +1,162 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyWetherApp extends StatefulWidget {
+  const MyWetherApp({super.key});
+
+  @override
+  State<MyWetherApp> createState() => _MyWetherAppState();
+}
+
+class _MyWetherAppState extends State<MyWetherApp> {
+  Map<String, dynamic> mydata = {};
+
+  void fetcherr() async {
+    try {
+      final ress = await http.get(Uri.parse(
+          'https://api.open-meteo.com/v1/forecast?latitude=9.025&longitude=38.7469&current=temperature_2m,is_day,rain&timezone=Europe%2FMoscow'));
+      if (ress.statusCode == 200) {
+        setState(() {
+          mydata = jsonDecode(ress.body);
+        });
+      } else {
+        throw Exception("error");
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetcherr();
+  }
+
+  Image imageselector() {
+    final wether = mydata['current']['temperature_2m'];
+    if (wether >= 20) {
+      return Image.asset('assets/sun.png');
+    } else {
+      return Image.asset('assets/sun.png');
+    }
+  }
+
+  bool backgroundselector() {
+    final day_night = mydata['current']['is_day'];
+    if (day_night == 1) {
+      ThemeData.dark();
+      return true;
+    } else {
+      ThemeData.dark();
+      return false;
+    }
+  }
+
+  String greetings() {
+    final day_night = mydata['current']['is_day'];
+    if (day_night == 1) {
+      return "Good Night";
+    } else {
+      return "Good morning";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final wether = mydata['current']['temperature_2m'];
+    final raim = mydata['current']['rain'];
+    final now = DateTime.now();
+    final formattedTime = DateFormat('h:mm a').format(DateTime.now());
+    final dayOfWeek = DateFormat('EEEE').format(now);
+
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Center(
+              child: mydata.isEmpty
+                  ? const Center(child: CircularProgressIndicator())
+                  : Center(
+                      child: Column(
+                        children: [
+                          Column(
+                            children: [
+                              const Text(
+                                'Addis Abeba',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                ),
+                              ),
+                              Text(
+                                ' $dayOfWeek    $formattedTime',
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              const SizedBox(height: 8),
+                              SizedBox(
+                                width: 300,
+                                child: imageselector(),
+                              ),
+                              const SizedBox(height: 100),
+                              Text(
+                                ' $wether °C ',
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              Text(
+                                greetings(),
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 70.0),
+                            child: Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.grey, // Icon color
+                                    ),
+                                    Text("rain \n $raim mm"),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.sunny,
+                                      color: Colors.grey, // Icon color
+                                    ),
+                                    Text("sunset \n $raim mm"),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.thermostat,
+                                      color: Colors.grey, // Icon color
+                                    ),
+                                    Text("temparure \n $wether c"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,26 @@
-import 'dart:convert';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_local_variable, non_constant_identifier_names
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:wetherapp/another.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Map<String, dynamic> data = {};
-
-  Future<void> fetcherr() async {
-    try {
-      final response = await http.get(Uri.parse(
-          'https://api.open-meteo.com/v1/forecast?latitude=9.025&longitude=38.7469&daily=temperature_2m_max&wind_speed_unit=ms&timezone=Europe%2FMoscow'));
-
-      if (response.statusCode == 200) {
-        setState(() {
-          data = jsonDecode(response.body);
-          print('Data fetched successfully: $data');
-        });
-      } else {
-        throw Exception('Failed to load data: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error fetching data: $e');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetcherr();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: const AssetImage("assets/sky.jpg"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.blue.withOpacity(0.0), BlendMode.darken)),
+    return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        // Dark Theme
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
       ),
-      child: ListView.builder(
-          itemCount: data['daily']['temperature_2m_max'].length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (BuildContext context, index) {
-            double temp = data['daily']['temperature_2m_max'][index];
-            String date = data['daily']['time'][index];
-            return Stack(children: [
-              Center(
-                child: data.isEmpty
-                    ? const Center(child: CircularProgressIndicator())
-                    : SizedBox(
-                        width: 300,
-                        child: ListTile(
-                          title: Container(
-                            decoration: BoxDecoration(
-                                color: const Color.fromARGB(179, 255, 255, 255)
-                                    .withOpacity(0.4),
-                                borderRadius: BorderRadius.circular(20)),
-                            height: 300,
-                            child: Center(
-                              child: Text(
-                                'Temperature: $temp  °C\n  date : $date',
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-              )
-            ]);
-          }),
-    ));
+      themeMode: ThemeMode.system, // Use the device theme
+      home: MyWetherApp(),
+    );
   }
 }
